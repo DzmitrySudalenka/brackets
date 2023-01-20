2c267be7a17b595e01dc4565d67b9baa71498e15
DzmitrySudalenka <sudalenko@gmail.com>
2023-01-20 14:26:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsPairs = {};
+  const closeBrackets = [];
+  bracketsConfig.forEach(pair => {
+    bracketsPairs[pair[0]] = pair[1];
+    closeBrackets.push(pair[1]);
+  });
+  let isStrContainsBracketsPair = true;
+  while (isStrContainsBracketsPair) {
+    isStrContainsBracketsPair = false;
+    for (let i = 0; i < str.length; i++) {
+      if (closeBrackets.includes(str[i])) {
+        if (bracketsPairs[str[i - 1]] == str[i]) {
+          str = str.split('');
+          str.splice(i, 1);
+          str.splice(i - 1, 1);
+          str = str.join('');
+          isStrContainsBracketsPair = true;
+          break;
+        }
+      }
+    }
+  }
+  return str ? false : true;
 }
